7df31a65814111fbd3406a83efa256d836acd62d
formigasantos0 <formigasantos0@gmail.com>
2026-09-21 14:46:27 -0300

    Minha mensagem de alteração

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-sexo = input("Informe o sexo [M/F]: ").strip().upper()
 
-while sexo != "M" and sexo != "F":
-  print("Valor inválido. Por favor, digite novamente.")
-  sexo = input("Informe o sexo [M/F]: ").strip().upper()
-
-if sexo == "M":
-  print("Sexo registrado com sucesso: Masculino.")
-else:
-  print("Sexo registrado com sucesso: Feminino.")
-  
 
 
